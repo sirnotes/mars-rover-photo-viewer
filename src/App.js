a1191a98a84b999bf1e770f9809e458ea108f595
Ken Nichol <ken.nichol@live.ca>
2019-06-25 15:49:09 -0700

    implement state and function injection

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import PhotoFinder from './components/PhotoFinder/PhotoFinder';
 import './App.css';
 
 const initialState = {
-
+  camera: '',
+  sol: 1,
+  maxSol: 2446,
+  photos: []
 }
 
 const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
@@ -16,12 +19,31 @@ class App extends Component {
     this.state = initialState;
   }
 
+  onCameraChange = (event) => {
+    this.setState({})
+  }
+
+  onSolChange = (event) => {
+    this.setState({sol: event.target.value})
+  }
+
+  onButtonSubmit = (event) => {
+
+  }
+
   render() {
+    const {maxSol, sol, photos} = this.state
     return (
       <div className="App">
         <div>
           <Logo />
-          <PhotoFinder apiUrl={apiUrl} />
+          <PhotoFinder
+            apiUrl={apiUrl}
+            maxSol={maxSol}
+            sol={sol}
+            onSolChange={this.onSolChange}
+            onButtonSubmit={this.onButtonSubmit}
+            onCameraChange={this.onCameraChange} />
         </div>
       </div>
     )
--- src/components/PhotoFinder/PhotoFinder.js
+++ src/components/PhotoFinder/PhotoFinder.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './PhotoFinder.css';
 
-const PhotoFinder = ({ onInputChange, onButtonSubmit, maxSol }) => {
+const PhotoFinder = ({ onSolChange, onCameraChange, onButtonSubmit, maxSol, sol }) => {
     return(
         <div>
             <p className="f3">
@@ -10,7 +10,7 @@ const PhotoFinder = ({ onInputChange, onButtonSubmit, maxSol }) => {
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <select className="f4 pa2 w-40 center" onChange={onInputChange}>
+                    <select className="f4 pa2 w-40 center" onChange={onCameraChange}>
                         <option value="FHAZ">Front Hazard Avoidance Camera</option>
                         <option value="RHAZ">Rear Hazard Avoidance Camera</option>
                         <option value="MAST">Mast Camera</option>
@@ -19,7 +19,7 @@ const PhotoFinder = ({ onInputChange, onButtonSubmit, maxSol }) => {
                         <option value="MARDI">Mars Descent Imager</option>
                         <option value="NAVCAM">Navigation Camera</option>
                     </select>
-                    <label>Mars Sol [1-{maxSol}] <input type="text" className="f4 pa2 w-20 center" onChange={onInputChange} /></label>
+                    <label>Mars Sol [1-{maxSol}] <input type="number" className="f4 pa2 w-20 center" onChange={onSolChange} max={maxSol} value={sol} /></label>
                     <button
                         className="w-30 grow f4 link ph3 pv2 dib white bg-light-red"
                         onClick={onButtonSubmit}
