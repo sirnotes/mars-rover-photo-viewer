463c6daf08aa5d6a9d3b6b546f90edb4f7d9dcb2
Ken Nichol <ken.nichol@live.ca>
2019-06-25 22:56:29 -0700

    Updates

---
+++ precache-manifest.2132c1b533184cd39c2d37606515231b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd36d115ec831ab13fc66b15bf0ae5d",
+    "url": "/mars-rover-photo-viewer/index.html"
+  },
+  {
+    "revision": "815732433ff9af85357d",
+    "url": "/mars-rover-photo-viewer/static/css/2.7727dc8f.chunk.css"
+  },
+  {
+    "revision": "b6d7f37f869351a18f94",
+    "url": "/mars-rover-photo-viewer/static/css/main.7ff2491f.chunk.css"
+  },
+  {
+    "revision": "815732433ff9af85357d",
+    "url": "/mars-rover-photo-viewer/static/js/2.d1a71c6c.chunk.js"
+  },
+  {
+    "revision": "b6d7f37f869351a18f94",
+    "url": "/mars-rover-photo-viewer/static/js/main.a61106e8.chunk.js"
+  },
+  {
+    "revision": "ce20ef0d55c6a9f3a391",
+    "url": "/mars-rover-photo-viewer/static/js/runtime~main.d8170f65.js"
+  },
+  {
+    "revision": "3def4fbb698dead67e29d6afa3ac6c8c",
+    "url": "/mars-rover-photo-viewer/static/media/mars.3def4fbb.svg"
+  }
+]);
