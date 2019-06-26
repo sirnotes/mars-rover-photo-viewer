eb35688258d7bbffc8ef9db4072c5e342479cf93
Ken Nichol <ken.nichol@live.ca>
2019-06-26 00:21:52 -0700

    register environment variables

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'tachyons';
 import './index.css';
 
+require('dotenv').config();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
